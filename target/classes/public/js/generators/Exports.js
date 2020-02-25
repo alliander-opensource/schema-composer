4b702855ea24001f6dde39b1424b351cb87e3d74
Haasjes <ruben.haasjes1@alliander.com>
2020-02-25 16:40:32 +0100

    Remove /target

--- target/classes/public/js/generators/Exports.js
+++
@@ -1,7 +0,0 @@
-function exportToSVG() {
-    var a = document.createElement('a');
-    var svg = canvas.toSVG().replace(/id="{.*?}"/g, "");
-    a.href = 'data:image/svg+xml,'+encodeURIComponent(svg);
-    a.download = 'schema.svg';
-    a.click();
-}
